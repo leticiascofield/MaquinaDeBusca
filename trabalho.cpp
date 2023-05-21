b3f5ea56154737371f4c48aa25d64f2584d6bb89
Letícia S. Lenzoni <125830543+leticiascofield@users.noreply.github.com>
2023-05-21 20:17:36 -0300

    normalizarTexto e wstring
    Ao fazer esse função, tive que mudar todos as strings para  wstring

--- trabalho.cpp
+++ trabalho.cpp
@@ -7,7 +7,7 @@ class MaquinaDeBusca{
     public:
 
     //Criar uma maquina de busca com documentos
-    MaquinaDeBusca(std::vector <std::string> documentos){
+    MaquinaDeBusca(std::vector <std::wstring> documentos){
         this->documentos = documentos;
 
         for (int i=0; i < documentos.size(); i++){
@@ -15,28 +15,60 @@ class MaquinaDeBusca{
         }
     }
 
-    std::vector <std::string> pesquisar(std::string textoPesquisado){
-        std::string pesquisaNormalizada = normalizarTexto(textoPesquisado);
-        std::vector <std::string> palavrasPesquisadas = separarPalavras(pesquisaNormalizada);
-        std::vector<std::map <std::string, int>> documentosComPalavra = procurarPalavra(palavrasPesquisadas, documentos);
+    std::vector <std::wstring> pesquisar(std::wstring textoPesquisado){
+        std::wstring pesquisaNormalizada = normalizarTexto(textoPesquisado);
+        std::vector <std::wstring> palavrasPesquisadas = separarPalavras(pesquisaNormalizada);
+        std::vector<std::map <std::wstring, int>> documentosComPalavra = procurarPalavra(palavrasPesquisadas, documentos);
         //Ainda falta resolver prioridade de documentos e retornar
         return 0;
     }
 
     //Para normalizar um texto (textoPesquisado e documentos)
-    std::string normalizarTexto(std::string texto){ //verificar função normalize()
-        std::string textoNormalizado;
-        //código
+    std::wstring normalizarTexto(std::wstring texto){
+
+        std::wstring textoNormalizado;
+        static const std::wstring maiusculaOuAcento = L"ABCDEFGHIJKLMNOPQRSTUVWXYZÀÁÂÃÄÅàáâãäåÇçÈÉÊËèéêëÌÍÎÏìíîïÒÓÔÕÖØòóôõöøÙÚÛÜùúûüÝý";
+        static const std::wstring caractereBasico = L"abcdefghijklmnopqrstuvwxyzaaaaaaaaaaaacceeeeeeeeiiiiiiiioooooooooooouuuuuuuuyy";
+        static const std::wstring alfabeto = L" abcdefghijklmnopqrstuvwxyz";
+        std::wstring aux;
+        aux.reserve(texto.length());
+
+        for (wchar_t c : texto) { //troca caracteres maiusculos ou com acento para o caractere basico
+            size_t found = maiusculaOuAcento.find(c);
+            if (found != std::wstring::npos) {
+                aux += caractereBasico[found];
+            } else {
+                aux += c;
+            }
+        }
+
+        int count = 0;
+        for(int i = 0; i < aux.size(); i++){ //exclui caracteres que não são letras ou espaço
+            for(int j = 0; j < alfabeto.size(); j++){
+                if(aux[i] == alfabeto[j]){
+                    count++;
+                }
+            }
+            if(count == 0){
+                aux.erase(aux.begin() + i);
+                i--;
+            }
+            count = 0;
+        }
+
+        for(int i = 0; i < aux.size(); i++){//coloca a auxiliar no textoNormalizado
+            textoNormalizado.push_back(aux[i]);
+        }
         return textoNormalizado;
     }
 
     //Para separar o texto pesquisado
-    std::vector <std::string> separarPalavras(std::string textoNormalizado){
+    std::vector <std::wstring> separarPalavras(std::wstring textoNormalizado){
 
-        std::vector <std::string> palavrasPesquisadas; //onde ficarão as palavras já separadas no vetor
+        std::vector <std::wstring> palavrasPesquisadas; //onde ficarão as palavras já separadas no vetor
 
         for(int i=0; i<textoNormalizado.size(); i++){ //itera pelo texto
-            std::string palavraAtual;
+            std::wstring palavraAtual;
             if(textoNormalizado[i+1] == ' ' || i+1 == textoNormalizado.size()){ //se a próxima posição for ' ' ou fim do texto, fazer:
                 palavraAtual.clear();
                 for(int j = i; textoNormalizado[j] != ' ' && j >= 0; j--){ //coloca palavra em palavraAtual (invertida)           
@@ -54,8 +86,8 @@ class MaquinaDeBusca{
     // Vector - palavra[i] que aparece nos documentos
     // Map - documento[j] e quantidade de vezes que tem palavra[i] (quantidade>0)
     //usar find()
-    std::vector<std::map <std::string, int>> procurarPalavra(std::vector<std::string> palavrasPesquisadas, std::vector <std::string> documentos){
-        std::vector<std::map <std::string, int>> documentosComPalavra; 
+    std::vector<std::map <std::wstring, int>> procurarPalavra(std::vector<std::wstring> palavrasPesquisadas, std::vector <std::wstring> documentos){
+        std::vector<std::map <std::wstring, int>> documentosComPalavra; 
         //código
         for (int i = 0; i < palavrasPesquisas.size(); i++){
             for (int j = 0; j < documentos.size(); j++)
@@ -67,16 +99,16 @@ class MaquinaDeBusca{
         return documentosComPalavra;
     }
 
-    //ver se tem todas as palavras em um docs -> fazer uma função (de map) para contar quantas vezes um string 
+    //ver se tem todas as palavras em um docs -> fazer uma função (de map) para contar quantas vezes um wstring 
     //(ex: d1.txt) aparece no vetor, se a quantidade for igual o tamanho do vetor, esse docs tem todas as palavras
 
     //somar os ints desse docs (que é um map), o que for maior aparece primeiro
     //if dois sejam iguais, aparecer o com nome menor
 
 
-    //Ainda tem que fazer funções do subsistema de recuperação e no main colocar os arquivos dos documentos no vector<string>
+    //Ainda tem que fazer funções do subsistema de recuperação e no main colocar os arquivos dos documentos no vector<wstring>
     private:
 
-    std::vector <std::string> documentos;
+    std::vector <std::wstring> documentos;
 
 };
