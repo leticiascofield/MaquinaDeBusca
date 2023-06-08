8aba7b501713c40d46906b1234ae5ff9fd7472ce
Juan Coelho <juancoelho25@hotmail.com>
2023-06-08 15:12:13 -0300

    Arquivo main.cpp alterado

--- main.cpp
+++ main.cpp
@@ -6,11 +6,29 @@
 
 int main() {
 
-    //colocar os arquivos dos documentos no vector<wstring> documentos
-    std::wstring textoPesquisado = "Eu estou pesquisando isso";
-    std::vector <std::wstring> documentos; //colocar os arquivos dos documentos
-    MaquinaDeBusca(documentos) buscaTeste; //quais documentos tem minha maquina de busca (no caso, todos)
-    buscaTeste.pesquisar(textoPesquisado);
+    std::vector<std::string> files; //Insere todos os arquivos de d1.txt até d30.txt
+    for (int i = 1; i <= 30; ++i) {
+        files.push_back("d" + std::to_string(i) + ".txt");
+    }
+
+    // Faz o índice invertido
+    std::map<std::string, std::map<std::string, int>> inverseIndex = buildInverseIndex(files);
+
+    // Abre o arquivo que contém todas as palavras de todos os arquivos
+    std::ofstream outputFile("output.txt");
+
+    // Escreve o índice invertido no arquivo output.txt
+    for (const auto& termEntry : inverseIndex) {
+        outputFile << termEntry.first << ":\n";
+        const std::map<std::string, int>& docMap = termEntry.second;
+
+        for (const auto& docEntry : docMap) {
+            outputFile << "   " << docEntry.first << ": " << docEntry.second << "\n";
+        }
+    }
+
+    // Fecha o arquivo output.txt
+    outputFile.close();
 
     return 0;
 }
